1e479b25ab6f7c13481df1ff97cff932849c9484
DrunkBearEKB <50179290+DrunkBearEKB@users.noreply.github.com>
2021-05-21 14:49:50 +0500

    Update exe_creator.bat

--- exe_creator.bat
+++ exe_creator.bat
@@ -1,1 +1,1 @@
-nuitka --mingw64 --windows-disable-console --windows-icon-from-ico resources\icon.ico screenshot_maker.py
+nuitka --mingw64 --windows-disable-console --windows-icon-from-ico resources\icon.ico screenshoter.py
